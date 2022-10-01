0a8153144a9b497254241f7a70425f012d65d46f
visarzejna <visarzejna@gmail.com>
2022-10-02 01:41:38 +0200

    Initial commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+import Banner from './Banner'
+
+function HomeScreen() {
+  return (
+    <div>
+      <Nav />
+      <Banner />
+    </div>
+    
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9smK7chA_kPKHOdaLnrjgLoND_vpr59U",
+    authDomain: "netflix-clone-1f482.firebaseapp.com",
+    projectId: "netflix-clone-1f482",
+    storageBucket: "netflix-clone-1f482.appspot.com",
+    messagingSenderId: "214882536320",
+    appId: "1:214882536320:web:cf1ad86607a4c8b050cf65"
+  };
